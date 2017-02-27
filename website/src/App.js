bb8d98b7be499761ff6f610df258ae30013b6dbf
Daniel Lin <danlin604@gmail.com>
2017-02-26 16:28:14 -0800

    forms and add item
    Signed-off-by: Daniel Lin <danlin604@gmail.com>

--- website/src/App.js
+++ website/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Projects from './Components/Projects';
+import AddProject from './Components/AddProject';
 import './App.css';
 
 class App extends Component {
@@ -7,27 +8,40 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      projects: [
-        {
-          title: "Business Website",
-          category: "Web Design"
-        },
-        {
-          title: "Social App",
-          category: "Mobile Development"
-        },
-        {
-          title: "Ecommerce Shopping Cart",
-          category: "Web Development"
-        }
-      ]
+      projects: []
     }
+  }
+
+  // lifecycle method, put data here
+  componentWillMount() {
+    this.setState({projects: [
+      {
+        title: "Business Website",
+        category: "Web Design"
+      },
+      {
+        title: "Social App",
+        category: "Mobile Development"
+      },
+      {
+        title: "Ecommerce Shopping Cart",
+        category: "Web Development"
+      }
+    ]});
+  }
+
+  handleAddProject(project) {
+    let projects = this.state.projects;
+    projects.push(project);
+    this.setState({projects:projects});
   }
 
   render() {
     return (
       <div className="App">
-        My app
+        <h1>My App</h1>
+        <AddProject addProject={this.handleAddProject.bind(this)}/>
+        <h1>Projects</h1>
         <Projects projects={this.state.projects} />
       </div>
     );
